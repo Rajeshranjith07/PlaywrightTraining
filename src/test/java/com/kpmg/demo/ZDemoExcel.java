a5c4dc858409f4ef09a1df85f703f2d0472923de
admin <admin@DESKTOP-7H5FTIU>
2026-01-20 13:42:01 +0530

    Excel data read and store

--- src/test/java/com/kpmg/demo/ZDemoExcel.java
+++ src/test/java/com/kpmg/demo/ZDemoExcel.java
@@ -1,8 +1,9 @@
 package com.kpmg.demo;
-
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -17,9 +18,9 @@ public class ZDemoExcel {
 		System.out.println(value);
 		
 		//write logic to print all cell values
-		for(int i=0;i<2;i++)
+		for(int i=1;i<3;i++)
 		{
-			for(int j=0;j<2;j++)
+			for(int j=0;j<3;j++)
 			{
 				String demoValue= sheet.getRow(i).getCell(j).getStringCellValue();
 				System.out.println(demoValue);
@@ -29,6 +30,39 @@ public class ZDemoExcel {
 		
 		book.close();
 		file.close();
+		
+		GetExcelData excelData=new GetExcelData();	
+		excelData.getExcelData();
+	}
+}
+	
+class GetExcelData{
+	public void getExcelData() throws IOException {
+
+		FileInputStream file = new FileInputStream("C:\\AutomationSession\\automation_workspace\\HybridFramework\\Test-Data\\orange-hrm-data.xlsx");
+
+		XSSFWorkbook book = new XSSFWorkbook(file);
+
+		XSSFSheet sheet = book.getSheet("invalidLoginTest");
+
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		int cellCount = sheet.getRow(0).getPhysicalNumberOfCells();
+
+		Object[][] data = new Object[rowCount - 1][cellCount];
+
+		DataFormatter format=new DataFormatter();
+		
+		for (int r = 1; r < rowCount; r++) {
+			for (int c = 0; c < cellCount; c++) {
+				data[r - 1][c] = format.formatCellValue(sheet.getRow(r).getCell(c));
+				
+			}
+		}
+		System.out.println(Arrays.deepToString(data));
+
+		book.close();
+		file.close();
 	}
 
 }
+
