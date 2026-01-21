644408d6408ba44752fee490f9d4cc190365a55e
admin <admin@DESKTOP-7H5FTIU>
2026-01-21 11:17:39 +0530

    page object

--- src/test/java/com/kpmg/test/LoginTest.java
+++ src/test/java/com/kpmg/test/LoginTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.kpmg.base.AutomationWrapper;
+import com.kpmg.pages.LoginPage;
 import com.kpmg.utilities.DataSource;
 
 public class LoginTest extends AutomationWrapper {
@@ -11,10 +12,9 @@ public class LoginTest extends AutomationWrapper {
 	@Test(dataProviderClass = DataSource.class,dataProvider = "commonDataProvider")
 	public void validLoginTest(String username, String password, String expectedValue)
 	{
-		page.locator("//input[@name='username']").fill(username);
-		page.locator("//input[@name='password']").fill(password);
+		LoginPage.enterUserName(page, username);
+		LoginPage.enterPassword(page, password);
 		page.locator("//button[contains(normalize-space(),'Login')]").click();
-		
 	    String actualValue=page.locator("//h6[contains(normalize-space(),'Dashb')]").innerText();
 	    Assert.assertEquals(actualValue, expectedValue);
 	
